784571913c693cf6c43fbba98fa63d3f3c044843
mpeng <michaelxpeng@gmail.com>
2018-09-12 00:49:38 -0400

    organized code/folders

--- models/teams.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Teams = sequelize.define("Teams", {
-    season: DataTypes.STRING,
-    teamName: DataTypes.STRING,
-    teamID: DataTypes.STRING,
-  });
-  Teams.sync();
-  return Teams;
-};
